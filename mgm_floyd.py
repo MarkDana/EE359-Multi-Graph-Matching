13c891274a13f78d661c8a984335b0f4f0869e79
MarkDana <dhy326@gmail.com>
2020-05-23 23:03:30 +0800

    ignore, TODO update

--- mgm_floyd.py
+++ mgm_floyd.py
@@ -2,6 +2,7 @@ import numpy as np
 
 LAMBDA = 0.3
 
+# TODO: pointwise performs better than matmul
 
 def cal_affinity_score(X, K):
     """
@@ -25,10 +26,9 @@ def cal_pairwise_consistency(X):
     n, _, m, _ = X.shape
     X_t = X.transpose((1, 0, 2, 3))
     # matmul:
-    pairwise_consistency = 1 - np.abs(X[:, :, None] - np.matmul(X[:, None], X_t[None, ...])).sum((2, 3, 4)) / (
-            2 * n * m)
+    # pairwise_consistency = 1 - np.abs(X[:, :, None] - np.matmul(X[:, None], X_t[None, ...])).sum((2, 3, 4)) / (2 * n * m)
     # point-wise:
-    # pairwise_consistency = 1 - np.abs(X[:, :, None] - X_t[None, ...] * X[:, None]).sum((2, 3, 4)) / (2 * n * m)
+    pairwise_consistency = 1 - np.abs(X[:, :, None] - X_t[None, ...] * X[:, None]).sum((2, 3, 4)) / (2 * n * m)
     return pairwise_consistency
 
 
@@ -41,7 +41,7 @@ def mgm_floyd(X, K, num_graph, num_node):
     :return: matching results, (num_graph, num_graph, num_node, num_node)
     """
     for k in range(num_graph):
-        Xopt = np.matmul(X[:, k][:, None], X[k, :][None, ...])
+        Xopt = np.matmul(X[:, None, k], X[None, k, :])
         Sorg = cal_affinity_score(X, K)
         Sopt = cal_affinity_score(Xopt, K)
         update = (Sopt > Sorg)[:, :, None, None]
@@ -49,7 +49,7 @@ def mgm_floyd(X, K, num_graph, num_node):
 
     for k in range(num_graph):
         pairwise_consistency = cal_pairwise_consistency(X)
-        Xopt = np.matmul(X[:, k][:, None], X[k, :][None, ...])
+        Xopt = np.matmul(X[:, None, k], X[None, k, :])
         Sorg = (1 - LAMBDA) * cal_affinity_score(X, K) + LAMBDA * pairwise_consistency
         Sopt = (1 - LAMBDA) * cal_affinity_score(Xopt, K) + LAMBDA * np.sqrt(  # sqrt pc for approximate
             np.matmul(pairwise_consistency[:, k][:, None], pairwise_consistency[k, :][None, ...]))
