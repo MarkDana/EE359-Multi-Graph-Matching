2762810acff5ebc89de5bc5ce6f2b39411004b6c
LiTrevize <universeofjimmy@gmail.com>
2020-05-24 22:59:19 +0800

    do not update self assignment

--- mgm_floyd.py
+++ mgm_floyd.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 LAMBDA = 0.3
+X0 = 1
 
 
 def cal_affinity_score(X, K):
@@ -10,10 +11,10 @@ def cal_affinity_score(X, K):
     :return: normalized affinity score, (num_graph, num_graph)
     """
     n, _, m, _ = X.shape
-    vx = np.reshape(X.transpose((0, 1, 3, 2)), newshape=(n, n, -1, 1))
+    vx = np.reshape(X, newshape=(n, n, -1, 1))
     vxT = vx.transpose((0, 1, 3, 2))
     affinity_score = np.matmul(np.matmul(vxT, K), vx)  # in shape (n, n, 1, 1)
-    normalized_affinity_score = affinity_score.reshape(n, n) / np.max(affinity_score)
+    normalized_affinity_score = affinity_score.reshape(n, n) / X0
     return normalized_affinity_score
 
 
@@ -40,25 +41,40 @@ def mgm_floyd(X, K, num_graph, num_node):
     :param num_node: number of node, int
     :return: matching results, (num_graph, num_graph, num_node, num_node)
     """
+    global X0
+    X0 = np.max(cal_affinity_score(X, K))
+
     for k in range(num_graph):
-        Xopt = np.matmul(X[:, k][:, None], X[k, :][None, ...])
+        Xopt = np.matmul(X[:, k, None], X[k, None, :])
         Sorg = cal_affinity_score(X, K)
         Sopt = cal_affinity_score(Xopt, K)
+
         update = (Sopt > Sorg)[:, :, None, None]
+        for i in range(num_graph):
+            update[i, i] = False
         X = update * Xopt + (1 - update) * X
+        # print(Xopt.shape, update.shape )
+
+        # for i in range(num_graph):
+        #     for j in range(num_graph):
+        #         if i != j and Sopt[i, j] > Sorg[i, j]:
+        #             X[i, j] = Xopt[i, j]
+        #
+        # assert (X1==X).all()
 
     for k in range(num_graph):
         pairwise_consistency = cal_pairwise_consistency(X)
-        Xopt = np.matmul(X[:, k][:, None], X[k, :][None, ...])
+        Xopt = np.matmul(X[:, k, None], X[k, None, :])
         Sorg = (1 - LAMBDA) * cal_affinity_score(X, K) + LAMBDA * pairwise_consistency
-        Sopt = (1 - LAMBDA) * cal_affinity_score(Xopt, K) + LAMBDA * np.sqrt(  # sqrt pc for approximate
-            np.matmul(pairwise_consistency[:, k][:, None], pairwise_consistency[k, :][None, ...]))
+        # Sopt = (1 - LAMBDA) * cal_affinity_score(Xopt, K,X0) + LAMBDA * np.sqrt(  # sqrt pc for approximate
+        #     np.matmul(pairwise_consistency[:, k][:, None], pairwise_consistency[k, :][None, ...]))
+        Sopt = (1 - LAMBDA) * cal_affinity_score(X, K) + LAMBDA * cal_pairwise_consistency(Xopt)
         update = (Sopt > Sorg)[:, :, None, None]
+        for i in range(num_graph):
+            update[i, i] = False
         X = update * Xopt + (1 - update) * X
 
+    for i in range(num_graph):
+        assert np.all(X[i, i] == np.eye(num_node)), X[i, i]
+
     return X
-
-
-if __name__ == '__main__':
-    X = np.random.randint(0, 10, (2, 2, 3, 3))
-    cal_pairwise_consistency(X)
